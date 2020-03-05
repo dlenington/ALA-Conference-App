bbe11706fcc9fbfe06074d6c3c0590393b7a239c
Dan Lenington <danlening02@gmail.com>
2020-03-05 17:47:48 -0600

    Update home
    add nav tabs for each day of conference

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,10 +1,33 @@
 import React, { Component } from "react";
-import Day from "../components/day";
+import AppBar from "@material-ui/core/AppBar";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
+import Typography from "@material-ui/core/Typography";
+import Box from "@material-ui/core/Box";
 
 class Home extends Component {
   state = {};
   render() {
-    return <Day />;
+    return (
+      <div>
+        <AppBar>
+          <Tabs value={value} onChange={handleChange}>
+            <Tab label="Item One" />
+            <Tab label="Item Two" />
+            <Tab label="Item Three" />
+          </Tabs>
+        </AppBar>
+        <TabPanel value={value} index={0}>
+          Item One
+        </TabPanel>
+        <TabPanel value={value} index={0}>
+          Item Two
+        </TabPanel>
+        <TabPanel value={value} index={0}>
+          Item Three
+        </TabPanel>
+      </div>
+    );
   }
 }
 
