b6fc594f7289f76bbe112530ba6bb2954acc2d04
Dan Lenington <danlening02@gmail.com>
2020-03-08 14:12:38 -0500

    Update

--- src/components/eventDialog.js
+++ src/components/eventDialog.js
@@ -1,5 +1,5 @@
-import React, { Component, Fragment } from 'react';
-import withStyles from '@material-ui/core/styles/withStyles';
+import React, { Component, Fragment } from "react";
+import withStyles from "@material-ui/core/styles/withStyles";
 import MyButton from "./myButton";
 
 //Mui stuff
@@ -10,47 +10,110 @@ import CircularProgress from "@material-ui/core/CircularProgress";
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
 
-
 //Icons
-import UnfoldMore from "@material-ui/icons/UnfoldMore"
+import UnfoldMore from "@material-ui/icons/UnfoldMore";
 import CloseIcon from "@material-ui/icons/Close";
 
+const styles = theme => ({
+  ...theme.spreadThis,
+  dialogContent: {
+    padding: 20
+  },
+  closeButton: {
+    position: "absolute",
+    left: "90%"
+  },
+  spinnerDiv: {
+    textAlign: "center",
+    marginTop: 50,
+    marginBottom: 50
+  }
+});
+
 class EventDialog extends Component {
-    state = {  }
-    render() { 
-        const {classes,
-            event: {
-                section,
-                title,
-                body:
-            }} = this.props;
-        return ( 
-            <Fragment>
-                <MyButton>
-                {/* <Button size="small" color="primary">
+  state = {
+    open: false,
+    oldPath: "",
+    newPath: ""
+  };
+
+  componentDidMount() {
+    if (this.props.openDialog) {
+      this.handleOpen();
+    }
+  }
+  handleOpen = () => {
+    let oldPath = window.location.pathname;
+    const { eventId } = this.props;
+    const newPath = `/events/${eventId}`;
+
+    this.setState({ open: true, oldPath, newPath });
+    this.props.getEvent(this.props.eventId);
+  };
+  handleClose = () => {
+    window.history.pushState(null, null, this.state.oldPath);
+    this.setState({ open: false });
+  };
+
+  render() {
+    const {
+      classes,
+      event: { section, title, venue, chair }
+    } = this.props;
+
+    const dialogMarkup = (
+      <Grid container spacing={2}>
+        <Grid item sm={7}>
+          <Typography color="primary" variant="h5">
+            {section}
+          </Typography>
+          <Typography color="primary" variant="h5">
+            {title}
+          </Typography>
+          <Typography color="primary" variant="h5">
+            {venue}
+          </Typography>
+          <Typography color="primary" variant="h5">
+            {chair}
+          </Typography>
+        </Grid>
+        <hr className={classes.invisibleSeparator} />
+        //render the presenters as comments
+        {/* {events.map => (event => { <Typography variant="body1">{event.presenter</Typography>} */}
+      </Grid>
+    );
+    return (
+      <Fragment>
+        <MyButton
+          onClick={this.handleOpen}
+          tip="Expand Event"
+          tipClassName={classes.expandButton}
+        >
+          {/* <Button size="small" color="primary">
             Learn More
           </Button> */}
-          <UnfoldMore color="primary"/>
-                </MyButton>
-                <Dialog open={this.state.open} onClose={this.handleClose}
-                fullWidth
-                maxWidth="sm"
-                >
-                    <MyButton
-                    tip="Close"
-                    onClick={this.handleClose}
-                    fullWidth
-                    maxWidth="sm"
-                    >
-                        <CloseIcon/>
-                    </MyButton>
-                    <DialogContent className={classes.dialogContent}>
-                        {dialogMarkup}
-                    </DialogContent>
-                </Dialog>
-            </Fragment>
-         );
-    }
+          <UnfoldMore color="primary" />
+        </MyButton>
+        <Dialog
+          open={this.state.open}
+          onClose={this.handleClose}
+          fullWidth
+          maxWidth="sm"
+        >
+          <MyButton
+            tip="Close"
+            onClick={this.handleClose}
+            tipClassName={classes.closeButton}
+          >
+            <CloseIcon />
+          </MyButton>
+          <DialogContent className={classes.dialogContent}>
+            {dialogMarkup}
+          </DialogContent>
+        </Dialog>
+      </Fragment>
+    );
+  }
 }
- 
-export default (withStyles)(styles)(EventDialog);
+
+export default withStyles(styles)(EventDialog);
--- src/components/myButton.js
+++ src/components/myButton.js
@@ -2,10 +2,10 @@ import React from "react";
 import Tooltip from "@material-ui/core/Tooltip";
 import IconButton from "@material-ui/core/IconButton";
 
-export default ({ children, onClick, tip, btnClassName, tipClassName }) => {
+export default ({ children, onClick, tip, btnClassName, tipClassName }) => (
   <Tooltip>
     <IconButton onClick={onClick} className={btnClassName}>
       {children}
     </IconButton>
-  </Tooltip>;
-};
+  </Tooltip>
+);
