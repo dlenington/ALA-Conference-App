81d99fee8214871713682455f715dfb666874085
Dan Lenington <danlening02@gmail.com>
2020-03-18 13:59:00 -0500

    Signup Page Update
    Update

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -1,7 +1,48 @@
 import React, { Component } from "react";
+import withStyles from "@material-ui/core/styles/withStyles";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+
+//Mui imports
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import CircularProgress from "@material-ui/core/CircularProgress";
 
 class Signup extends Component {
-  state = {};
+  constructor() {
+    super();
+    this.state = {
+      email: "",
+      password: "",
+      confirmPassword: "",
+      handle: "",
+      errors: {},
+      fieldErrors: {}
+    };
+  }
+
+  setAuthorizationHeader = token => {
+    const FBIdToken = `Bearer ${token}`;
+    localStorage.setItem("FBIdToken", FBIdToken);
+    axios.defaults.headers.common["Authorization"] = FBIdToken;
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    this.setState({
+      loading: true
+    });
+
+    const newUserData = {
+      email: this.state.email,
+      password: this.state.password,
+      confirmPassword: this.state.confirmPassword,
+      handle: this.state.handle
+    };
+    axios.post("/signup", newUserData).then(res => {});
+  };
   render() {
     return (
       <div>
