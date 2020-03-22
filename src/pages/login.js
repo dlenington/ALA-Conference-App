5800d69fc69f89fa60c6383a3bd945f07679ffbb
Dan Lenington <danlening02@gmail.com>
2020-03-22 11:24:50 -0500

    Login page

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,14 +1,79 @@
 import React, { Component } from "react";
+import withStyles from "@material-ui/core/styles/withStyles";
+import axios from "axios";
+import { Link } from "react-router-dom";
+
+//MUI stuff
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+
+const styles = theme => ({
+  ...theme.spreadThis,
+  form: {
+    textAlign: "center"
+  },
+  textField: {
+    margin: "10px auto 10px auto"
+  },
+  button: {
+    marginTop: 20,
+    position: "relative"
+  }
+})
 
 class Login extends Component {
-  state = {};
+  state = {
+    email: "",
+    password: "",
+    errors: {}
+  };
+
+  loginUser = userData => {
+    axios
+    .post("/login", userData)
+    .then(res => {
+      const FBIdToken = `Bearer ${res.data.token}`;
+      localStorage.setItem("FBIdToken", FBIdToken);
+      axios.defaults.headers.common["Authorization"] = FBIdToken;
+      this.props.history.push("/");
+    })
+    .catch(err => {
+    this.setState({
+      errors: err.response.data
+    });
+    });
+  };
+  handleSubmit = event => {
+    event.preventDefault();
+    const userData = {
+      email: this.state.email,
+      password: this.state.password
+    };
+    this.loginUser(userData);
+  }
+
+  handleChange = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  };
+
   render() {
+    const { classes } = this.props;
+    const { errors } = this.state;
     return (
-      <div>
-        <p>Login</p>
-      </div>
+      <Grid item sm />
+      <Grid item sm >
+<Typography variant="h2">Login</Typography>
+<form noValidate onSubmit={this.handleSubmit}>
+  
+</form>
+      </Grid>
+      <Grid item sm />
     );
   }
 }
 
-export default Login;
+export default withStyles(styles)(Login);
