488cea421093e983aaefc4864e277a54c1708aa5
Dan Lenington <danlening02@gmail.com>
2020-03-25 09:50:35 -0500

    Redux Reducers
    Update and add reducer

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,17 @@
 import { createStore, combineReducers, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 
+import userReducer from "./reducers/userReducer";
+import dataReducer from "./reducers/uiReducer";
+import uiReducer from "./reducers/dataReducer";
+
 const initialState = {};
 
-const reducers = combineReducers({});
+const reducers = combineReducers({
+  user: userReducer,
+  data: dataReducer,
+  UI: uiReducer
+});
 
 const store = createStore(reducers, initialState);
 
