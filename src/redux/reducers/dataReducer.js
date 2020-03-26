43c5f7f78b1af59029e70440eacdb7e65d765347
Dan Lenington <danlening02@gmail.com>
2020-03-26 10:29:26 -0500

    Update dataReducer.js

--- src/redux/reducers/dataReducer.js
+++ src/redux/reducers/dataReducer.js
@@ -11,5 +11,25 @@ export default function(state = initial, action) {
         ...state,
         loading: true
       };
+    case SET_EVENTS:
+      return {
+        ...state,
+        posts: action.payload,
+        loading: false
+      };
+    case SET_POST:
+      return {
+        ...state,
+        post: action.payload
+      };
+    case LIKE_EVENT:
+    case UNLIKE_EVENT:
+      let index = state.events.findIndex(
+        event => event.eventId === action.payload.eventId
+      );
+      state.events[index] = action.payload;
+      if (state.post.postId === action.payload.postId) {
+        state.event.likeCount = action.payload.likeCount;
+      }
   }
 }
