088762bd76032ab06e4a8f29fe1b0b778850820f
Dan Lenington <danlening02@gmail.com>
2020-03-28 12:05:42 -0500

    Update Redux Types

---
+++ src/redux/types.js
@@ -0,0 +1,17 @@
+export const SET_AUTHENTICATED = "SET_AUTHENTICATED";
+export const SET_UNAUTHENTICATED = "SET_UNAUTHENTICATED";
+export const SET_USER = "SET_USER";
+export const LOADING_USER = "LOADING_USER";
+export const SET_ADMIN = "SET_ADMIN";
+//UI reducer types
+export const SET_ERRORS = "SET_ERRORS";
+export const LOADING_UI = "LOADING_UI";
+export const CLEAR_ERRORS = "CLEAR_ERRORS";
+export const LOADING_DATA = "LOADING_DATA";
+export const STOP_LOADING_UI = "STOP_LOADING_UI";
+
+//Data Reducer Types
+export const SET_EVENTS = "SET_EVENTS";
+export const SET_EVENT = "SET_EVENT";
+export const LIKE_EVENT = "LIKE_EVENT";
+export const UNLIKE_EVENT = "UNLIKE_EVENT";
