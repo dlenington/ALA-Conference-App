068216b0ea1a591728aed9aea411780309d7224c
Dan Lenington <danlening02@gmail.com>
2020-04-02 17:35:31 -0500

    Update userActions.js

---
+++ src/redux/actions/userActions.js
@@ -0,0 +1,18 @@
+import {
+  SET_USER,
+  SET_ERRORS,
+  CLEAR_ERRORS,
+  LOADING_UI,
+  SET_UNAUTHENTICATED
+} from "../types";
+
+import axios from "axios";
+
+export const loginUser = (userData, history) => dispatch => {
+  dispatch({ type: LOADING_UI });
+  axios.post("/login", userData).then(res => {
+    setAuthorizationHeader(res.data.token);
+    dispatch(getUserData());
+    dispatch({});
+  });
+};
