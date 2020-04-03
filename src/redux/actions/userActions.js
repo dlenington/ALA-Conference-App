809ac375a982c3b937671d1be8ab048bb7ec2435
Dan Lenington <danlening02@gmail.com>
2020-04-03 17:02:22 -0500

    Update userActions.js

--- src/redux/actions/userActions.js
+++ src/redux/actions/userActions.js
@@ -3,17 +3,23 @@ import {
   SET_ERRORS,
   CLEAR_ERRORS,
   LOADING_UI,
-  SET_UNAUTHENTICATED
+  SET_UNAUTHENTICATED,
 } from "../types";
 
 import axios from "axios";
 
-export const loginUser = (userData, history) => dispatch => {
+export const loginUser = (userData, history) => (dispatch) => {
   dispatch({ type: LOADING_UI });
-  axios.post("/login", userData).then(res => {
+  axios.post("/login", userData).then((res) => {
     setAuthorizationHeader(res.data.token);
     dispatch(getUserData());
     dispatch({ type: CLEAR_ERRORS });
     history.push("/");
   });
+};
+
+export const logoutUser = () => (dispatch) => {
+  localStorage.removeItem("FBIdToken");
+  delete axios.defaults.headers.common["Authorization"];
+  dispatch({ type: SET_UNAUTHENTICATED });
 };
