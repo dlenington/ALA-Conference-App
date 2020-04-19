365edb146c0f1afd8a43b87d05d9ea3ce2941b51
Dan Lenington <danlening02@gmail.com>
2020-04-19 12:07:47 -0500

    Update eventDialog.js

--- src/components/eventDialog.js
+++ src/components/eventDialog.js
@@ -77,29 +77,6 @@ class EventDialog extends Component {
     } = this.props;
 
     if (panels) {
-      const dialogMarkup = panels.map((panel) => (
-        <Fragment>
-          <Grid container spacing={2}>
-            <Grid item sm={7}>
-              <Typography color="primary" variant="h5">
-                {panel.section}
-              </Typography>
-              <Typography color="textSecondary" variant="h6">
-                {panel.title}
-              </Typography>
-              <Typography color="textSecondary" variant="body2">
-                {panel.venue}
-              </Typography>
-              <Typography color="textSecondary" variant="body2">
-                Chair: {panel.chair}
-              </Typography>
-            </Grid>
-          </Grid>
-          <hr className={classes.invisibleSeparator} />
-          <PanelDetails panelId={panel.panelId} />
-          <hr className={classes.invisibleSeparator} />
-        </Fragment>
-      ));
     }
 
     return (
