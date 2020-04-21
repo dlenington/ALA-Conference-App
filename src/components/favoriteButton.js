85ccfabf0702bf5c48596dc1e3223abfe932bcdb
Dan Lenington <danlening02@gmail.com>
2020-04-21 09:13:31 -0500

    Update favoriteButton.js

--- src/components/favoriteButton.js
+++ src/components/favoriteButton.js
@@ -3,11 +3,28 @@ import React, { Component } from 'react';
 
 //Redux Stuff
 
-class LikeButton extends Component {
+class FavoriteButton extends Component {
     state = {  }
     render() { 
+        const { authenticated } = this.props.user;
+        const favoriteButton = !authenticated ? (
+            <Link to="/login">
+                <MyButton tip="Favorite">
+                    <FavoriteBorder color="primary" />
+                </MyButton>
+            </Link>
+        ) : this.likedPainting() ? (
+            <MyButton tip="Undo like" onClick={this.unlikeEvent}>
+                <FavoriteBorder color="primary" />
+            </MyButton>
+        ) : (
+            <MyButton itp="Like" onClick={this.likeEvent}>
+                <FavoriteBorder color="primary" />
+            </MyButton>
+        )
+        
         return (  );
     }
 }
  
-export default ;
+export default FavoriteButton;
