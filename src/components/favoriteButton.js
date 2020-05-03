713ad852cf2ba07a1293083a065d8147183708a0
Dan Lenington <danlening02@gmail.com>
2020-05-03 15:33:27 -0500

    Update favoriteButton.js

--- src/components/favoriteButton.js
+++ src/components/favoriteButton.js
@@ -13,59 +13,59 @@ import { likeEvent, unlikeEvent } from "../redux/actions/dataActions";
 
 class FavoriteButton extends Component {
   state = {};
-  // likedEvent = () => {
-  //   if (
-  //     this.props.user.likes &&
-  //     this.props.user.likes.find((like) => like.panelId === this.props.panelId)
-  //   )
-  //     return true;
-  //   else return false;
-  // };
+  likedEvent = () => {
+    if (
+      this.props.user.likes &&
+      this.props.user.likes.find((like) => like.panelId === this.props.panelId)
+    )
+      return true;
+    else return false;
+  };
 
-  // likeEvent = () => {
-  //   this.props.likeEvent(this.props.panelId);
-  // };
-  // unlikeEvent = () => {
-  //   this.props.unlikeEvent(this.props.panelId);
-  // };
+  likeEvent = () => {
+    this.props.likeEvent(this.props.panelId);
+  };
+  unlikeEvent = () => {
+    this.props.unlikeEvent(this.props.panelId);
+  };
 
   render() {
-    // const { authenticated } = this.props.user;
+    const { authenticated } = this.props.user;
 
-    // const favoriteButton = !authenticated ? (
-    //   <Link to="/login">
-    //     <MyButton tip="Favorite">
-    //       <FavoriteBorder color="primary" />
-    //     </MyButton>
-    //   </Link>
-    // ) : this.likedEvent() ? (
-    //   <MyButton tip="Undo like" onClick={this.unlikeEvent}>
-    //     <FavoriteIcon color="primary" />
-    //   </MyButton>
-    // ) : (
-    //   <MyButton tip="Like" onClick={this.likeEvent}>
-    //     <FavoriteBorder color="primary" />
-    //   </MyButton>
-    // );
+    const favoriteButton = !authenticated ? (
+      <Link to="/login">
+        <MyButton tip="Favorite">
+          <FavoriteBorder color="primary" />
+        </MyButton>
+      </Link>
+    ) : this.likedEvent() ? (
+      <MyButton tip="Undo like" onClick={this.unlikeEvent}>
+        <FavoriteIcon color="primary" />
+      </MyButton>
+    ) : (
+      <MyButton tip="Like" onClick={this.likeEvent}>
+        <FavoriteBorder color="primary" />
+      </MyButton>
+    );
 
-    return <h1>HEllo</h1>;
+    return favoriteButton;
   }
 }
 
-// FavoriteButton.propTypes = {
-//   user: PropTypes.object.isRequired,
-//   panelId: PropTypes.string.isRequired,
-//   likeEvent: PropTypes.func.isRequired,
-//   unlikeEvent: PropTypes.func.isRequired,
-// };
+FavoriteButton.propTypes = {
+  user: PropTypes.object.isRequired,
+  panelId: PropTypes.string.isRequired,
+  likeEvent: PropTypes.func.isRequired,
+  unlikeEvent: PropTypes.func.isRequired,
+};
 
-// const mapStateToProps = (state) => ({
-//   user: state.user,
-// });
+const mapStateToProps = (state) => ({
+  user: state.user,
+});
 
-// const mapActionsToProps = {
-//   likeEvent,
-//   unlikeEvent,
-// };
+const mapActionsToProps = {
+  likeEvent,
+  unlikeEvent,
+};
 
-export default FavoriteButton;
+export default connect(mapStateToProps, mapActionsToProps)(FavoriteButton);
