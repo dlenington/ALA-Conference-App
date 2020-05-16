0468b3264a03010d4baf17e6bf2dbf83df4b315a
Dan Lenington <danlening02@gmail.com>
2020-05-16 11:59:04 -0500

    Remove Redux Reducers
    Remove redux reducers

--- src/redux/reducers/dataReducer.js
+++ src/redux/reducers/dataReducer.js
@@ -32,28 +32,19 @@ export default function (state = initialState, action) {
         event: action.payload,
       };
 
-    //Need to add the panelId to the likes array for the user in redux store.
-
-    //Alternatively, when the like or unlike event is called, we can call SET_USER to reset the likes array for the user.
-    case LIKE_EVENT:
-    case UNLIKE_EVENT:
-      // let index = state.events.findIndex(
-      //   (event) => event.eventId === action.payload.eventId
-      // );
-      // state.events[index] = action.payload;
-      // if (state.event.eventId === action.payload.eventId) {
-      //   state.event.likeCount = action.payload.likeCount;
-      // }
-
-      state.user.likes.push(action.payload);
-
-      return {
-        ...state,
-      };
-    case SET_LIKES:
-      return {
-      state.user.likes[...action.payload.likes]
-      };
+    //Updates the likecount for events. Not necessary for this application
+    // case LIKE_EVENT:
+    // case UNLIKE_EVENT:
+    //   // let index = state.events.findIndex(
+    //   //   (event) => event.eventId === action.payload.eventId
+    //   // );
+    //   // state.events[index] = action.payload;
+    //   // if (state.event.eventId === action.payload.eventId) {
+    //   //   state.event.likeCount = action.payload.likeCount;
+    //   // }
+    //   return {
+    //     ...state,
+    //   };
     default:
       return state;
   }
