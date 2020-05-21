56e2cb42f6c864e431ea31ecda9265a1d9101b24
Dan Lenington <danlening02@gmail.com>
2020-05-21 09:08:16 -0500

    Update favorites.js

--- src/pages/favorites.js
+++ src/pages/favorites.js
@@ -1,51 +1,29 @@
 import React, { Component } from "react";
 
+//Redux stuff
+import { connect } from "react-redux";
+
+//MUI Stuff
+import Grid from "@material-ui/core/Grid";
+
 class Favorites extends Component {
   render() {
     const { events } = this.props.data;
     let eventsMarkup = events.map((event) => (
       <Event key={event.eventId} event={event} />
     ));
-    return  <Grid container spacing={2}>
-    <Grid item sm={12} xs={12}>
-      {eventsMarkup}
-    </Grid>
-  </Grid>;
-  }
-}
-
-export default Favorites;
-
-import React, { Component, Fragment } from "react";
-import axios from "axios";
-import Event from "./event";
-import PropTypes from "prop-types";
-
-//Redux stuff
-import { getEvents } from "../redux/actions/dataActions";
-import { connect } from "react-redux";
-
-//Mui Stuff
-import Grid from "@material-ui/core/Grid";
-
-class Day extends Component {
-  componentDidMount() {
-    const { day } = this.props;
-    this.props.getEvents(day);
-  }
-  render() {
     return (
-     
+      <Grid container spacing={2}>
+        <Grid item sm={12} xs={12}>
+          {eventsMarkup}
+        </Grid>
+      </Grid>
     );
   }
 }
 
-Day.propTypes = {
-  getEvents: PropTypes.func.isRequired,
-  data: PropTypes.object.isRequired,
-};
-
 const mapStateToProps = (state) => ({
+  user: state.user,
   data: state.data,
 });
 
