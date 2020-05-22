bc2dfeb2acc14165f11c7abead96509edc6816b7
Dan Lenington <danlening02@gmail.com>
2020-05-22 09:03:44 -0500

    Update favorites.js

--- src/pages/favorites.js
+++ src/pages/favorites.js
@@ -21,33 +21,35 @@ class Favorites extends Component {
       <Grid container spacing={2}>
         <Grid item sm={12} xs={12}>
           {/* {eventsMarkup} */}
-          <Card>
-            <CardContent>
-              <Typography variant="h5">Student/Parent Survey</Typography>
-              <Chart
-                width={"500px"}
-                height={"300px"}
-                chartType="Bar"
-                loader={<div>Loading Chart</div>}
-                data={[
-                  ["Year", "Sales", "Expenses", "Profit"],
-                  ["2014", 1000, 400, 200],
-                  ["2015", 1170, 460, 250],
-                  ["2016", 660, 1120, 300],
-                  ["2017", 1030, 540, 350],
-                ]}
-                options={{
-                  // Material design options
-                  chart: {
-                    title: "Company Performance",
-                    subtitle: "Sales, Expenses, and Profit: 2014-2017",
-                  },
-                }}
-                // For tests
-                rootProps={{ "data-testid": "2" }}
-              />
-            </CardContent>
-          </Card>
+          <div width="auto">
+            <Card>
+              <CardContent>
+                <Typography variant="h5">Student/Parent Survey</Typography>
+                <Chart
+                  width={"500px"}
+                  height={"300px"}
+                  chartType="Bar"
+                  loader={<div>Loading Chart</div>}
+                  data={[
+                    ["Year", "Sales", "Expenses", "Profit"],
+                    ["2014", 1000, 400, 200],
+                    ["2015", 1170, 460, 250],
+                    ["2016", 660, 1120, 300],
+                    ["2017", 1030, 540, 350],
+                  ]}
+                  options={{
+                    // Material design options
+                    chart: {
+                      title: "Company Performance",
+                      subtitle: "Sales, Expenses, and Profit: 2014-2017",
+                    },
+                  }}
+                  // For tests
+                  rootProps={{ "data-testid": "2" }}
+                />
+              </CardContent>
+            </Card>
+          </div>
         </Grid>
       </Grid>
     );
