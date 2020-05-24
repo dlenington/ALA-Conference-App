4c9403c8dd4476ee3d2fa73a389f2b7b5aa8dd19
Dan Lenington <danlening02@gmail.com>
2020-05-23 19:03:10 -0500

    Update favorites.js

--- src/pages/favorites.js
+++ src/pages/favorites.js
@@ -33,7 +33,6 @@ class Favorites extends Component {
                   data={[
                     ["Year", "Sales", "Expenses", "Profit"],
                     ["2014", 1000, 400, 200],
-                    ["2015", 1170, 460, 250],
                     ["2016", 660, 1120, 300],
                     ["2017", 1030, 540, 350],
                   ]}
